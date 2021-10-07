b44b016d374bd7b75f73f52ca18b5a41193d99b6
renu katiyar <renu katiyar@DESKTOP-34H6K4F>
2021-10-07 23:57:33 +0530

    Add project files.

---
+++ TCS/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCS.Controllers
+{
+        public class AccountController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+             
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult AddApp()
+        {
+            return View();
+        }
+
+        public ActionResult AddAppVersions()
+        {
+            return View();
+        }
+
+        public ActionResult DownloadAnalysis()
+        {
+            return View();
+        }
+        public ActionResult AddVersions()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
